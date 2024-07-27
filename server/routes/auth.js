b229f1d9b9704bf77f0c3142d7a7d800b1fddddf
jeslindevelopment <158544156+jeslindevelopment@users.noreply.github.com>
2024-07-27 19:39:10 +0530

    added swagger and routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,16 +6,8 @@ const auth = require('../controllers/authController')
 // ======= ROUTE FOR /API/AUTH/ ============
 router.post('/login', auth.signin)
 router.post('/logout', afterSignupAuth, auth.logout)
-router.post('/loginAsUser', afterSignupAuth, auth.signinAsUser)
 router.post('/signup', auth.validate(), auth.signup)
-router.get('/:email', auth.checkEmail)
-router.get('/checkusername/:username', auth.checkUsername)
-router.post('/forgotpassword', checkValidHost, auth.sendRecoveryEmail)
-router.post('/forgotpassword/change', auth.changePassword)
-router.post('/forgotpassword/check', auth.checkRecoveryCode)
-// router.post('/config', auth.config)
-router.post('/sendConfirmationRegister', auth.generateCodeForRegister)
-router.post('/verifyAccountRegister', auth.verifyAccountRegister)
-router.post('/verifyGoogleCaptcha', auth.verifyGoogleCaptcha)
+router.post('/forgotpassword', auth.forgotPassword)
+
 
 module.exports = router
